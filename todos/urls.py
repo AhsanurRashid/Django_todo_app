f9fe22551a33080088817605138c09ef6054ff89
AhsanurRashid <ahasan.noor09@gmail.com>
2026-05-01 19:14:01 +0600

    Deleting task

--- todos/urls.py
+++ todos/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('addTask/', views.add_task, name='add_task'),
+    path('markAsDone/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+    path('markAsUndone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
+    path('deleteTask/<int:pk>/', views.delete_task, name='delete_task'),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Task
 
 # Create your views here.
 def add_task(request):
     if request.method == 'POST':
         task = request.POST.get('task')
+        if not task:
+            return redirect('home')
         # save the task to the database
         Task.objects.create(task=task)
         return redirect('home')
+    
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
+
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('home')
